d4119781f8bd241273fd3b9c4af371c670520305
rckolaczek <28630584+rckolaczek@users.noreply.github.com>
2026-06-08 17:02:28 -0400

    Move all helpers operations to functions. Standardize main execution flow.

--- fanControl.py
+++ fanControl.py
@@ -3,9 +3,8 @@ import json
 import redfish
 import keyring
 
+# authentication
 login_account = 'admin'
-
-# Retrieve the password from the keyring
 password = keyring.get_password('redfish', login_account)
 login_host = f'https://{keyring.get_password('redfish', 'host')}'
 
@@ -17,12 +16,7 @@ if not login_host:
     login_host = input(f'Enter password for {login_host}: ')
     keyring.set_password('redfish', 'host', login_host)
 
-
-# main
-REDFISH_OBJ = redfish.redfish_client(base_url=login_host, username=login_account, \
-                      password=password, default_prefix='/redfish/v1/')
-REDFISH_OBJ.login(auth="session")
-
+# helpers
 def get_odata_spec(REDFISH_OBJ):
     response = REDFISH_OBJ.get("/redfish/v1/JsonSchemas/GBTFanprofileService.v1_0_0.json", None)
     return response
@@ -40,26 +34,38 @@ def get_fan_profile(REDFISH_OBJ):
     response = REDFISH_OBJ.get("/redfish/v1/Chassis/Self/Thermal/FanprofileService/Fanprofile", None).dict
     return response
 
-# change the active fan profile | dump to file
-fan_profile = get_fan_profile(REDFISH_OBJ)
-headers = {'Content-Type': 'multipart/form-data'}
-new_profile = 'Unraid-Default-GPU'
-fan_profile['strMode'] = new_profile
-with open('fan_profiles.json', 'w') as f:
-    json.dump(fan_profile, f)
+def dump_fan_profile(REDFISH_OBJ, new_mode):
+    fan_profile = get_fan_profile(REDFISH_OBJ)
+    fan_profile['strMode'] = new_mode
+    with open('fan_profiles.json', 'w') as f:
+        json.dump(fan_profile, f)
 
-# import the new profile
-with open('fan_profiles.json', 'rb') as file_stream:
-    payload = {
-        'ImportFanprofile': ('fan_profiles.json', file_stream, 'application/octet-stream')
-    }
-    response = REDFISH_OBJ.post("/redfish/v1/Chassis/Self/Thermal/FanprofileService/Fanprofile", body=payload, headers=headers)
+def set_fan_profile(REDFISH_OBJ, profile_file):
+    headers = {'Content-Type': 'multipart/form-data'}
+    with open(profile_file, 'rb') as file_stream:
+        payload = {
+            'ImportFanprofile': ('fan_profiles.json', file_stream, 'application/octet-stream')
+        }
+        response = REDFISH_OBJ.post("/redfish/v1/Chassis/Self/Thermal/FanprofileService/Fanprofile", body=payload, headers=headers)
+    return response
+
+
+# main
+REDFISH_OBJ = redfish.redfish_client(base_url=login_host, username=login_account, \
+                      password=password, default_prefix='/redfish/v1/')
+REDFISH_OBJ.login(auth="session")
+
+# dump fan profile | update active profile | set new profile
+new_mode = 'Unraid-Default-GPU'
+dump_fan_profile(REDFISH_OBJ, new_mode)
+set_fan_profile(REDFISH_OBJ, 'fan_profiles.json')
+REDFISH_OBJ.logout()
 
 
 
-# print the response
+# print the response | remove later
 # sys.stdout.write("%s\n" % response)
-print(response)
+# print(response)
 # print(response.dict)
 # print(temperatures)
 # print(fan_profiles)
@@ -71,6 +77,3 @@ print(response)
 # # print fan profiles
 # for policy in fan_profiles:
 #     print(f"Profile Name: {policy['strName']}")
-
-
-REDFISH_OBJ.logout()
